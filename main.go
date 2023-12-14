28262de0f977b06eccc785a9e06c9b03da2fa4b9
Kieran <kieran.webdevelopment@gmail.com>
2023-12-14 12:06:01 +0000

    added profanity checker

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
 	"log"
 	"net/http"
 
@@ -12,6 +16,10 @@ type apiConfig struct {
 }
 
 func main() {
+	fmt.Println("Server starting...")
+	cleanChirp := profanityChecker("This is a kerfuffle opinion I need to share with the world")
+	log.Println(cleanChirp)
+
 	const filepathRoot = "."
 	const port = "8080"
 
@@ -26,8 +34,8 @@ func main() {
 
 	apiRouter := chi.NewRouter()
 	apiRouter.Get("/healthz", handlerReadiness)
-	// apiRouter.Get("/metrics", apiCfg.handlerMetrics)
 	apiRouter.Get("/reset", apiCfg.handlerReset)
+	apiRouter.Post("/validate_chirp", handlerChirpsValidate)
 	router.Mount("/api", apiRouter)
 
 	adminRouter := chi.NewRouter()
@@ -43,4 +51,84 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type returnVals struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+	// type returnValsChecked struct {
+	// 	CleanedBody string `json:"cleaned_body"`
+	// }
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
+		return
+	}
+
+	const maxChirpLength = 140
+	if len(params.Body) > maxChirpLength {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	cleanedChirp := profanityChecker(params.Body)
+
+	respondWithJSON(w, http.StatusOK, returnVals{
+		CleanedBody: cleanedChirp,
+	})
+
+	// if isProfanity {
+	// 	respondWithJSON(w, http.StatusOK, returnVals{
+	// 		CleanedBody: cleanedChirp,
+	// 	})
+	// } else {
+	// 	respondWithJSON(w, http.StatusOK, returnValsValid{
+	// 		Valid: true,
+	// 	})
+	// }
+
+}
+
+func profanityChecker(chirp string) string {
+	chirpArray := strings.Split(chirp, " ")
+
+	for i, word := range chirpArray {
+		lowerWord := strings.ToLower(word)
+		if lowerWord == "kerfuffle" || lowerWord == "sharbert" || lowerWord == "fornax" {
+			chirpArray[i] = "****"
+		}
+	}
+	cleanedChirp := strings.Join(chirpArray, " ")
+	return cleanedChirp
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Printf("Responding with 5XX error: %s", msg)
+	}
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errorResponse{
+		Error: msg,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(dat)
 }
